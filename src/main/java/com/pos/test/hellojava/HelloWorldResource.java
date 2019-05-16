105d879b1501b8cb320cf0e7141efce75b1fbe91
sarath <sreddy13@jcp.com>
2019-05-16 15:10:53 +0000

    Update

--- src/main/java/com/pos/test/hellojava/HelloWorldResource.java
+++ src/main/java/com/pos/test/hellojava/HelloWorldResource.java
@@ -5,27 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping(path = "/")
 public class HelloWorldResource {
 
     @Value("${message: POS}")
     private String message;
 
-    @GetMapping(path = "/hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/hello-world")
     public ResponseEntity sayHelloToPOS() {
         String returnMsg = "Hello "+message+"! This is Java client";
         return new ResponseEntity(returnMsg, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/admin/health", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity healthStatus() {
-        return new ResponseEntity("UP", HttpStatus.OK);
-    }
 
-    @GetMapping(path = "/admin/info", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getInfo() {
-        return new ResponseEntity("Hello World client built in Java", HttpStatus.OK);
-    }
 }
